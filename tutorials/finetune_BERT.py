172161ffb164593808c2e8ec0c6757b9ac9c8cdd
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2023-08-11 19:16:28 +0100

    Pushing test script7

--- tutorials/finetune_BERT.py
+++ tutorials/finetune_BERT.py
@@ -18,9 +18,9 @@ def tokenize_function(examples):
 
 BATCH_SIZE = 4
 
-tokenized_datasets = dataset.map(tokenize_function, batched=False)
+tokenized_datasets = dataset.map(tokenize_function, batched=True, batch_size=BATCH_SIZE)
 
-train_dataloader = DataLoader(tokenized_datasets["train"], shuffle=True, batch_size=BATCH_SIZE)
+# train_dataloader = DataLoader(tokenized_datasets["train"], shuffle=True, batch_size=BATCH_SIZE)
 
 
 # Load the model
@@ -38,40 +38,18 @@ optimizer = optim.AdamW(model.parameters(), lr=5e-5)
 EPOCHS = 1
 
 for epoch in range(EPOCHS):
-    for batch in train_dataloader:
+    for batch in tokenized_datasets['train']:
         optimizer.zero_grad()
 
-        # Convert list of tensors to tensors
-        print(batch['input_ids'][0])
-        if torch.is_tensor(batch['input_ids'][0]):
-            input_ids = torch.stack(batch['input_ids'])
-        else:
-            input_ids = torch.stack([t.to(device) for t in batch['input_ids']])
-
-        if torch.is_tensor(batch['attention_mask'][0]):
-            attention_mask = torch.stack(batch['attention_mask'])
-
-        else:
-            attention_mask = torch.stack([t.to(device) for t in batch['attention_mask']])
-
-        if torch.is_tensor(batch['label'][0]):
-            labels = torch.stack(batch['label'])
-        else:
-            labels = torch.stack([t.to(device) for t in batch['label']])
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['label'].to(device)
 
         # Move all to device
         input_ids = input_ids.to(device)
         attention_mask = attention_mask.to(device)
         labels = labels.to(device)
 
-
-        # input_ids = torch.stack([t.to(device) for t in batch['input_ids']])
-        # attention_mask = torch.stack([t.to(device) for t in batch['attention_mask']])
-        # # labels = torch.stack([t.to(device) for t in batch['label']])
-        # #
-        # # input_ids = batch['input_ids'].to(device)
-        # # attention_mask = batch['attention_mask'].to(device)
-        # labels = batch['label'].to(device)
 
         outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
         loss = outputs.loss
