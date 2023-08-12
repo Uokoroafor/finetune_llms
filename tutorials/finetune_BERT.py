e4042d752874b32fb50f2e8545bd2123cd2f7359
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2023-08-12 22:33:14 +0100

    Pushing test script10

--- tutorials/finetune_BERT.py
+++ tutorials/finetune_BERT.py
@@ -64,7 +64,7 @@ for param in model.classifier.parameters():
 batch_size = 8
 dataloader = DataLoader(dataset, sampler=RandomSampler(dataset), batch_size=batch_size)
 optimizer = AdamW(model.parameters(), lr=3e-5, eps=1e-8)
-epochs = 3
+epochs = 50
 scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=len(dataloader) * epochs)
 
 loss_fn = MSELoss()
@@ -73,6 +73,7 @@ model.train()
 start_time = time.time()
 for epoch in range(epochs):
     epoch_start = time.time()
+    epoch_loss = 0
     for idx, batch in enumerate(dataloader):
         input_ids = batch[0].to(device)
         attention_masks = batch[1].to(device)
@@ -81,19 +82,31 @@ for epoch in range(epochs):
         optimizer.zero_grad()
         outputs = model(input_ids, attention_mask=attention_masks)[0].squeeze()
         loss = loss_fn(outputs, labels)
+        epoch_loss += loss.item()
         loss.backward()
         optimizer.step()
         scheduler.step()
         # print every 100 steps
         if (idx + 1) % 100 == 0:
             print(f'Batch_ID: {idx + 1}, Epoch: {epoch + 1}, Loss:  {loss.item():,.4f}')
+
+    print(f'Epoch {epoch + 1} complete! Loss: {epoch_loss / (idx + 1):,.4f}')
     epoch_end = time.time()
 
     hours = (epoch_end - epoch_start) // 3600
     minutes = ((epoch_end - epoch_start) % 3600) // 60
     seconds = (epoch_end - epoch_start) % 60
-    print(f'Epoch{epoch + 1} complete in {hours} hours, {minutes} minutes and {seconds} seconds.')
+    print(f'Epoch{epoch + 1} complete in {hours} hour(s), {minutes} minute(s) and {seconds: .2f} seconds.')
+
+# Total training time
+end_time = time.time()
+hours = (end_time - start_time) // 3600
+minutes = ((end_time - start_time) % 3600) // 60
+seconds = (end_time - start_time) % 60
+print(f'Training complete in {hours} hour(s), {minutes} minute(s) and {seconds: .2f} seconds.')
+
 
 # Save the model
 model.save_pretrained("./my_bert_regression_model/")
 tokenizer.save_pretrained("./my_bert_regression_model/")
+print("Model saved!")
